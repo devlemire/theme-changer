93fc80173c5371951ef2e0373c70d50ba9bfcd90
James Lemire <webdev.jameslemire@gmail.com>
2017-05-01 12:37:21 -0600

    JL solution - Update solution

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 export default class ColorChanger extends Component {
-  componentWillMount() {
-    console.log('MOUNTING')
-  }
-
   render() {
     return (
       <div>
